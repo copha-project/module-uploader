2a65c0a099d6de9580aa2f5689bf1049685cdcfd
aong <yxwzaxns@gmail.com>
2022-01-26 18:50:39 +0800

    add fetch() with net

--- src/common.ts
+++ src/common.ts
@@ -1,9 +1,57 @@
 import os from 'os'
+import { urlToHttpOptions, URL } from 'url'
 import { validate } from 'uuid'
+import {net, ClientRequestConstructorOptions} from 'electron'
 
 export const isWin32 = os.platform() === 'win32'
 export const isMac = os.platform() === 'darwin'
 export const isDev = process.env.NODE_ENV === "development"
 export function isUUID(s: string) {
     return validate(s)
+}
+
+export async function fetch(url:string, data?:any, options?:ClientRequestConstructorOptions){
+    return new Promise((resolve,reject)=>{
+        const urlData = urlToHttpOptions(new URL(url)) as ClientRequestConstructorOptions
+        const request = net.request({
+            ...urlData,
+            ...options
+        })
+        const resp = {
+            code: 200,
+            headers: {},
+            body: {}
+        }
+        request.on('response', (response) => {
+            resp.code = response.statusCode
+            resp.headers = JSON.stringify(response.headers)
+            response.on('data', (chunk) => {
+                try {
+                    resp.body = JSON.parse(Buffer.from(chunk).toString())
+                } catch (error) {
+                    resp.body = Buffer.from(chunk).toString()
+                }
+            })  
+            response.on('error',(e)=>{
+                console.log('response error',e);
+                reject(e)
+            })
+            response.on('end', () => {
+                if(resp.code === 200){
+                    resolve(resp.body)
+                }else{
+                    reject(resp.body)
+                }
+            })
+        })
+        request.on('abort',(e)=>{
+            console.log('abort',e);
+            reject(e)
+        })
+        request.on('error',(e)=>{
+            console.log('error',e);
+            reject(e)
+        })
+        request.end()
+    })
 }
